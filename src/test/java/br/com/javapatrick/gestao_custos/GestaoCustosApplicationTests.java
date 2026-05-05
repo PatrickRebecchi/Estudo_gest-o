b5f2a2d18d650aaeddd3fd5a4e358ef5981e91f7
Patrick <patrickrebecchi@hotmail.com>
2026-05-04 22:24:03 -0300

    Criando projeto

---
+++ src/test/java/br/com/javapatrick/gestao_custos/GestaoCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javapatrick.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
